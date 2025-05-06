c4bb5b08d8197232a9f5b7e5abcb5ed645ba538b
averywsc <159727750+averywsc@users.noreply.github.com>
2025-05-06 14:19:11 +1200

    week 1 done

---
+++ milestone_1/milestone_1_code.py
@@ -0,0 +1,3 @@
+age = int(input("How old are you?")) #asks user their age
+name = input("What is your name?")   #asks user their name
+print("Hello {} you will be {} years old in 2050".format(name, age + 25))
